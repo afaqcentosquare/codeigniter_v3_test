5b93f8c1e1e1090dc1c7e3136f1de317ee51fd8b
CentoSquare <afaq.centosquare@gmail.com>
2022-06-13 16:49:26 +0500

    Task-6 completed

--- application/models/ProductModel.php
+++ application/models/ProductModel.php
@@ -51,6 +51,73 @@ class ProductModel extends CI_Model {
         return $query->result();
     }
 
+    public function getNonAttachProducts()
+    {
+        $sql = "SELECT `product_id` FROM `orders`";
+        $query_result = $this->db->query($sql);
+        $order_result = $query_result->result_array();
+        $arr = array_map(function($value){
+            return $value['product_id'];
+        },$order_result);
+        $query = $this->db->select('*')->from('products');
+        $result = $query->where_not_in('id', $arr)->get();
+        return $result->result_array();
+    }
+
+    public function quantityOfAllActiveAttachedProducts()
+    {
+        $total_quantity = 0;
+        $active_products = $this->db->get_where('products', array('status' => 'active'));
+        $active_product_result = $active_products->result_array();
+        $arr = array_map(function($value){
+            return $value['id'];
+        },$active_product_result);
+
+        if(count($arr) > 0){
+            $this->db->select("sum(quantity) as total_quantity");
+            $this->db->from('orders');
+            $this->db->where_in('product_id', $arr);
+            $result_data = $this->db->get()->result_array();
+            foreach($result_data as $data){
+                $total_quantity += $data['total_quantity'];
+            }
+        }
+        
+        return $total_quantity;
+    }
+
+    public function amountOfAllActiveAttachedProducts()
+    {
+        $total_amount = 0;
+        $active_products = $this->db->get_where('products', array('status' => 'active'));
+        $active_product_result = $active_products->result_array();
+        $arr = array_map(function($value){
+            return $value['id'];
+        },$active_product_result);
+
+        if(count($arr) > 0){
+            $this->db->select("sum(quantity * price) as total_active_price");
+            $this->db->from('orders');
+            $this->db->where_in('product_id', $arr);
+            $result_data = $this->db->get()->result_array();
+            foreach($result_data as $data){
+                $total_amount += $data['total_active_price'];
+            }
+        }
+        
+        return $total_amount;
+    }
+
+    public function productsInfoByUser()
+    {
+            $this->db->select("name, user_id, sum(quantity * price) as total_price");
+            $this->db->from('orders');
+            $this->db->group_by('user_id');
+            $this->db->join('users', 'orders.user_id = users.id');
+            $result_data = $this->db->get()->result_array();
+            return $result_data;
+    }
+
 }
 
 ?>
--- application/views/admin/dashboard.php
+++ application/views/admin/dashboard.php
@@ -17,7 +17,7 @@
                 <div class="card">
                     <div class="card-body">
                         <h4 class="card-title">Count of all active and verified users.</h4>
-                        <p class="card-text"></p>
+                        <p class="card-text"><?php echo $data['active_count'] ?></p>
                     </div>
                 </div>
             </div>
@@ -25,7 +25,7 @@
                 <div class="card">
                     <div class="card-body">
                         <h4 class="card-title">Count of active and verified users who have attached active products.</h4>
-                        <p class="card-text"></p>
+                        <p class="card-text"><?php echo $data['attached_product_user_count'] ?></p>
                     </div>
                 </div>
             </div>
@@ -33,7 +33,7 @@
                 <div class="card">
                     <div class="card-body">
                         <h4 class="card-title">Count of all active products (just from products table).</h4>
-                        <p class="card-text"></p>
+                        <p class="card-text"><?php echo $data['active_products'] ?></p>
                     </div>
                 </div>
             </div>
@@ -41,13 +41,49 @@
                 <div class="card">
                     <div class="card-body">
                         <h4 class="card-title">Count of active products which don't belong to any user.</h4>
-                        <p class="card-text"></p>
+                        <p class="card-text"><?php echo $data['non_attach_products'] ?></p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        <h4 class="card-title">Quantity Ordered of all active attached products.</h4>
+                        <p class="card-text"><?php echo $data['total_quantity_attach_products'] ?></p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        <h4 class="card-title">Amount of all active attached products.</h4>
+                        <p class="card-text"><?php echo $data['total_amount_of_attach_products'] ?></p>
                     </div>
                 </div>
             </div>
         </div>
         <a href="<?php echo site_url('index.php/admin/product/create');?>" class="btn btn-primary">Add Product</a>
     </div>
+
+    <div class="container mt-3">
+  <h2>Product Table</h2>          
+  <table class="table table-bordered">
+    <thead>
+      <tr>
+        <th>User Name</th>
+        <th>Total Price</th>
+      </tr>
+    </thead>
+    <tbody>
+        <?php foreach($data['product_info_by_user'] as $product) { ?>
+      <tr>
+        <td><?php echo $product['name'] ?></td>
+        <td><?php echo $product['total_price'] ?></td>
+      </tr>
+      <?php } ?>
+    </tbody>
+  </table>
+</div>
     
     <!-- Bootstrap JS -->
     <script src="https://www.markuptag.com/bootstrap/5/js/bootstrap.bundle.min.js"></script>
